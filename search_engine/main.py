d0815c41ac14ac3810f923f962a1817e10ff1e98
Joshua Nichols <58839751+JoshuaNichols1@users.noreply.github.com>
2023-12-17 14:54:08 +1000

    ok

---
+++ search_engine/main.py
@@ -0,0 +1,20 @@
+#this is a project where I'll create a search engine which can take a query and
+#return results from the web. These results will be ranked using a custom ai
+#model. The model will be trained using a dataset of 10,000 websites and their
+#placements in google search results.
+
+#this is the main file which will run the program
+
+import math
+import os
+import sys
+import time
+import requests
+import json
+from flask import Flask, render_template, request, redirect, url_for, session
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
